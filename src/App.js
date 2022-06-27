538a561d81751a0a92ae4d764ad34de4c4fbce29
Raymond Nehring <rnehring89@gmail.com>
2022-06-27 18:13:22 -0500

    outlet router example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Home from './routes/home/home.component'
 import {Routes, Route} from 'react-router-dom'
 
 
+const Shop = () => {
+  return <h2>I am the shop</h2>
+}
+
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />}>
+        <Route path='/shop' element={<Shop />} />
+      </Route>
     </Routes>
     );
 
